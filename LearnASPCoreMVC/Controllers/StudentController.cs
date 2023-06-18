70388ca68ba5d4eedc40d71d73040ef0b637030c
AbhiDashadiya <pabhi1504@gmail.com>
2023-06-18 11:18:18 +0530

    video 8 complete : student controller made using scaffolding.
    Note : we have to chnage this controller because it will not show the data like student joined on which courses

---
+++ LearnASPCoreMVC/Controllers/StudentController.cs
@@ -0,0 +1,158 @@
+﻿using LearnASPCoreMVC.Data;
+using LearnASPCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnASPCoreMVC.Controllers
+{
+	public class StudentController : Controller
+	{
+		private readonly DataContext _context;
+
+		public StudentController(DataContext context)
+		{
+			_context = context;
+		}
+
+		// GET: Student
+		public async Task<IActionResult> Index()
+		{
+			return _context.Students != null ?
+						View(await _context.Students.ToListAsync()) :
+						Problem("Entity set 'DataContext.Students'  is null.");
+		}
+
+		// GET: Student/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.Students == null)
+			{
+				return NotFound();
+			}
+
+			var student = await _context.Students
+				.FirstOrDefaultAsync(m => m.StudentID == id);
+			if (student == null)
+			{
+				return NotFound();
+			}
+
+			return View(student);
+		}
+
+		// GET: Student/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Student/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("StudentID,Name,Enrolled")] Student student)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(student);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(student);
+		}
+
+		// GET: Student/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.Students == null)
+			{
+				return NotFound();
+			}
+
+			var student = await _context.Students.FindAsync(id);
+			if (student == null)
+			{
+				return NotFound();
+			}
+			return View(student);
+		}
+
+		// POST: Student/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("StudentID,Name,Enrolled")] Student student)
+		{
+			if (id != student.StudentID)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(student);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!StudentExists(student.StudentID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(student);
+		}
+
+		// GET: Student/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.Students == null)
+			{
+				return NotFound();
+			}
+
+			var student = await _context.Students
+				.FirstOrDefaultAsync(m => m.StudentID == id);
+			if (student == null)
+			{
+				return NotFound();
+			}
+
+			return View(student);
+		}
+
+		// POST: Student/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.Students == null)
+			{
+				return Problem("Entity set 'DataContext.Students'  is null.");
+			}
+			var student = await _context.Students.FindAsync(id);
+			if (student != null)
+			{
+				_context.Students.Remove(student);
+			}
+
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool StudentExists(int id)
+		{
+			return (_context.Students?.Any(e => e.StudentID == id)).GetValueOrDefault();
+		}
+	}
+}
