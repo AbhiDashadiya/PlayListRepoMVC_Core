1beaae3d287deeea1a3e07834a8e512b804f8f71
AbhiDashadiya <pabhi1504@gmail.com>
2023-06-19 23:08:42 +0530

    video 20 complete : query string passing
    Note : new controller added for test

---
+++ LearnASPCoreMVC/Controllers/QueryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnASPCoreMVC.Controllers
+{
+    public class QueryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowData(string first)
+        {
+            ViewBag.First = first;
+            return View();
+        }
+
+    }
+}
